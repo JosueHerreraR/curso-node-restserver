25b0f9c9200e374bc14849e8c701203195a2f302
Joel Josue Herrera Reyes <92177163+JosueHerreraR@users.noreply.github.com>
2022-12-22 12:35:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
 
-// 104. Iniciando el proyecto - RESTServer
-
-/*
-require('dotenv').config();
-
-const express = require('express')
-const app = express()
-
-app.get('/', function (req, res) {
-    res.send('Home Page')
-});
-
-app.listen(process.env.PORT, () => {
-    console.log('Servidor corriendo en el puerto:', process.env.PORT);
-});
-*/
-
-// 105. EXPRESS basado en clases
+// EXPRESS basado en clases
 
 require('dotenv').config();
 const Server = require('./models/server');
